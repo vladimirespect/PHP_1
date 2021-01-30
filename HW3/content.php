07f6076bdecdf5ff4b423ddae3e85ea4fe829f27
Vladimir <vladimirespect@mail.ru>
2021-01-30 16:11:32 +0300

    Added HW3

---
+++ HW3/content.php
@@ -0,0 +1,7 @@
+<h1> Some content</h1>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.<br>
+    Ab blanditiis excepturi, id illo maxime nemo nisi pariatur quis. <br>
+    Accusantium aliquam, animi aspernatur debitis deleniti doloribus,<br>
+    exercitationem itaque perferendis possimus sit, ullam veniam. <br>
+    Adipisci, aspernatur atque, culpa dignissimos earum eligendi est et in quae, <br>
+    rem repellendus repudiandae similique soluta tempora ullam?</p>
